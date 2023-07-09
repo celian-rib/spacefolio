d24732473340a50761e72733901d9e5c56e4c666
celian-rib <celian.riboulet@gmail.com>
2023-07-09 15:29:16 +0200

    Better styling

---
+++ src/parallax/asteroids.ts
@@ -0,0 +1,12 @@
+import Parrallax from "./parallax";
+
+function createAsteroidElement(element: HTMLElement) {
+    const asteroid = document.createElement('div');
+    asteroid.classList.add('asteroid');
+    return asteroid;
+}
+
+export default function startAsteroidLoop(parallax: Parrallax) {
+    
+
+}
