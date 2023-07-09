07f324c3f0fdd78741a7aab0106973838f20ae36
celian-rib <celian.riboulet@gmail.com>
2023-07-09 21:21:48 +0200

    Planets

--- src/main.ts
+++ src/main.ts
@@ -18,19 +18,38 @@ parallax.addIdToLayer('dir-2', 1);
 parallax.addIdToLayer('dir-3', 1);
 parallax.addIdToLayer('dir-4', 1);
 
-const directions = document.querySelectorAll('.direction p');
+parallax.addIdToLayer('bg-gaz-planet', 3);
+parallax.addIdToLayer('bg-black-hole', 3);
+
+parallax.addIdToLayer('bg-asteroid-1', 2);
+parallax.addIdToLayer('bg-asteroid-2', 2);
+
+const directions = document.querySelectorAll('.direction');
 
 document.addEventListener('mousemove', (e) => {
   for (let i = 0; i < directions.length; i++) {
     const direction = directions[i];
+
+    // get image in direction
+    const planetImg = direction.querySelector('img')!;
     const pos = {
-      x: direction.getBoundingClientRect().x,
-      y: direction.getBoundingClientRect().y,
+      x: planetImg.getBoundingClientRect().x + (planetImg.width / 2),
+      y: planetImg.getBoundingClientRect().y + (planetImg.height / 2),
     }
 
-    const distanceToMouse = Math.sqrt(Math.pow(e.clientX - pos.x, 2) + Math.pow(e.clientY - pos.y, 2));
+    const planetRadius = planetImg.width / 4;
 
-    const distText = Math.floor(Math.max(Math.pow(distanceToMouse - 10, 2), 0));
-    direction.innerHTML = `${distText}<span>km</span>`;
+    let distanceToMouse = Math.sqrt(Math.pow(e.clientX - pos.x, 2) + Math.pow(e.clientY - pos.y, 2)) - planetRadius;
+    const text = direction.querySelector('p')!;
+
+    if (distanceToMouse < planetRadius) {
+      text.innerHTML = `click to land`;
+    } else {
+      distanceToMouse = Math.max((distanceToMouse * 10) - 500, 0);
+
+      const distText = Math.floor(distanceToMouse);
+
+      text.innerHTML = `${distText}<span>km</span>`;
+    }
   }
 });
